7d5e81acc0a8cbe813321851a982b89134eb9993
Vishnu-Hande <134043231+Vishnu-Hande@users.noreply.github.com>
2023-06-27 01:02:47 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,21 @@
+package day1;
+
+import java.util.Scanner;
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float per=0;
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter Subject 1 marks : ");
+		int sub1=in.nextInt();
+		System.out.print("Enter Subject 2 marks : ");
+		int sub2=in.nextInt();
+		System.out.print("Enter Subject 3 marks : ");
+		int sub3=in.nextInt();
+		
+		per=(((float)(sub1+sub2+sub3))*100)/300;
+		System.out.printf(" Percentage = %.2f",per);
+	}
+
+}
